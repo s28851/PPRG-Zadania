929adbeafeb338f917ca2e97a9251fcbd54bc333
s28851 <116391818+s28851@users.noreply.github.com>
2022-11-18 23:35:40 +0100

    Zadanie_3_4.cpp
    Zadanie_3_4.cpp

--- Zadanie_3_4.cpp
+++ Zadanie_3_4.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <iomanip>
 
+using namespace std;
+
+int liczba(int i, int j)
+{
+    if (j == 1 || j == i)
+        return 1;
+    else
+        return liczba(i - 1, j - 1) + liczba(i - 1, j);
+}
+
+int main()
+{
+    int iloscwierszy;
+    cout << "Program drukuje Trojkat Pascala." << endl <<"Podaj liczbę wierszy do wydrukowania w zakresie od 1 do 20 :";
+    cin >> iloscwierszy;
+    cout << endl << "wiersz" <<endl;
+    for (int i = 1; i <= iloscwierszy; i++)
+    {
+        cout << setw(2) << i << setw (4 * ( iloscwierszy - i)) << "";
+        for (int j = 1; j <= i; j++)
+            cout << setw(8) << liczba(i, j);
+        cout << endl;
+    }
+    return 0;
+}
