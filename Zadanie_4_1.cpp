3dbcca46c4b600b7c16240b558f5662d82da39da
s28851 <116391818+s28851@users.noreply.github.com>
2022-12-17 16:49:44 +0100

    Create Zadanie_4_1.cpp
    Zadanie 4.1

---
+++ Zadanie_4_1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a=0;
+    
+    cout << "Podaj liczbe calkowita od 1 do 30 okreslajaca ilosc elementow tablicy:" << endl;
+    cin >> a;
+    cout << endl;
+    int tabl[a];
+    
+    cout<<"Podaj kolejno " << a <<" liczb calkowitych, po kazdej nacisnij ENTER:";
+    
+    cout << endl;
+    int j=0;
+    int b;
+    do 
+    {
+        cin >> b;
+        tabl[j]=b;
+        j++;
+    } while (j<a);
+    if (a==1) {
+        cout <<"Jedyna i najwieksza liczba to " << tabl[0] <<endl;
+        return 0;
+    }
+    else 
+    {
+        int naj=0;
+        for (int i=0;i<a;i++)
+        {
+            if (naj<tabl[i]) { naj=tabl[i] ;}
+        }
+        cout<<"Najwieksza liczba w tablicy to " << naj <<endl;
+    }
+    return 0;
+}
