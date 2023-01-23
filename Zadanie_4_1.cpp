49e7b67adae9c9f1e5d22d882561b22834beeae4
Marek <md@mediman.pl>
2023-01-23 19:51:16 +0100

    zatiwerdzone

--- Zadanie_4_1.cpp
+++ Zadanie_4_1.cpp
@@ -3,49 +3,46 @@ using namespace std;
 
 int main()
 {
-    int a=0;
-    
+    int a = 0;
+
     cout << "Podaj liczbe calkowita od 1 do 30 okreslajaca ilosc elementow tablicy:" << endl;
     cin >> a;
     cout << endl;
     int tabl[a];
-    int najtabl[a];
     int iloscnaj = 0;
-    
-    cout<<"Podaj kolejno " << a <<" liczb calkowitych, po kazdej nacisnij ENTER:";
-    
+
+    cout << "Podaj kolejno " << a << " liczb calkowitych, po kazdej nacisnij ENTER:";
+
     cout << endl;
-    int j=0;
+    int j = 0;
     int b;
-    do 
+    do
     {
         cin >> b;
-        tabl[j]=b;
+        tabl[j] = b;
         j++;
-    } while (j<a);
-    if (a==1) {
-        cout <<"Jedyna i najwieksza liczba to " << tabl[0] <<endl;
+    } while (j < a);
+
+    if (a == 1) {
+        cout << "Jedyna i najwieksza liczba to " << tabl[0] << endl;
         return 0;
     }
-    else 
+    else
     {
-        int naj=0;
-        for (int i=0;i<a;i++)
-        {
-            if (naj<tabl[i]) { naj=tabl[i]}
-            if (naj=tabl[i]) { najtabl[iloscnaj]=tabl[i]}
-            iloscnaj++;
+        int naj = 0;
+        for (int i = 0; i < a; i++) {
+
+            if (naj == tabl[i]) { iloscnaj++; }
+            if (naj < tabl[i]) { naj = tabl[i]; iloscnaj = 1; }
+
         }
-        if (iloscnaj = 1) {
+
+        if (iloscnaj == 1) {
             cout << "Najwieksza liczba w tablicy to " << naj << endl;
         }
         else {
-            cout << "Jest " << iloscnaj << " najwiekszych liczb. Sa to kolejno: "
-                for (i = 0; i < iloscnaj; i++) {
-                    cout << najtabl[i] << " ";
-                }
-
-            cout << endl << "Koniec programu" << endl;
+            cout << "Najwieksza liczba w tablicy to " << naj << endl;
+            cout << "Wystapila " << iloscnaj << " razy. " << endl << "Koniec programu, dziêkujê" << endl;
         }
     }
     return 0;
