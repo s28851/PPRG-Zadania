9273c12d850772ca3d3dd2f170314580e60f95dd
s28851 <116391818+s28851@users.noreply.github.com>
2023-01-28 00:44:02 +0100

    Zadanie_6_4

--- Zadanie_6_4.cpp
+++ Zadanie_6_4.cpp
@@ -10,7 +10,7 @@ int main()
     string c;
     
     cout<<"Program tworzący wielokrotną konakenację ciągu podanego przez użytkownika"<<endl<<endl<<"Podaj ciąg znaków: ";
-    cin >> a;
+    getline (cin, a);
     cout << "Podaj liczbę powtózeń tekstu: ";
     cin >> b;
     
