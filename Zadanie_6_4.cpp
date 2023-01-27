8587ab3460b789d168c962f0976387c51f584d37
s28851 <116391818+s28851@users.noreply.github.com>
2023-01-28 00:02:13 +0100

    Zadanie_6_4

--- Zadanie_6_4.cpp
+++ Zadanie_6_4.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main()
+{
+    string a;
+    int b;
+    string c;
+    
+    cout<<"Program tworzący wielokrotną konakenację ciągu podanego przez użytkownika"<<endl<<endl<<"Podaj ciąg znaków: ";
+    cin >> a;
+    cout << "Podaj liczbę powtózeń tekstu: ";
+    cin >> b;
+    
+    for (int i=0;i<b;i++){
+        c=c+a;
+    }
+     cout << c;
+     
+     cout <<endl << "To już jest koniec... :) MD";
+
+    return 0;
+}
